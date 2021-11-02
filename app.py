3a742b6e2884550df2d9a1912877852b6450bf87
Esteban Helguero <esteban.helguero@redk.net>
2021-11-02 07:58:16 +0100

    Add post put and delete methods

--- app.py
+++ app.py
@@ -18,3 +18,37 @@ def getProduct(product_name):
         return jsonify({ "products": productsFound})
     else:
         return jsonify({ "message": "Product not found"})
+
+@app.route('/products', methods=['GET', 'POST'])
+def addProduct():
+    new_product = {
+        "name": request.json["name"],
+        "price": request.json["price"],
+        "quantity": request.json["quantity"]
+    }
+    products.append(new_product)
+    return jsonify(products)
+
+@app.route('/products/<string:product_name>', methods=['DELETE'])
+def deleteProduct(product_name):
+    productsFound = [product for product in products if product["name"] == product_name]
+    if(len(productsFound) > 0):
+        product.remove(productsFound[0])
+        return jsonify({ 
+            "message": "Deleted product", products": products
+        })
+    return jsonify({ "message": "Product not found"})
+
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productsFound = [product for product in products if product["name"] == product_name]
+    if len(productsFound) > 0:
+        productsFound[0]["name"] = request.json["name"]
+        productsFound[0]["price"] = request.json["price"]
+        productsFound[0]["quantity"] = request.json["quantity"]
+
+        return jsonify({ 
+            "message": "Updated product", products": products
+            })
+    return jsonify({ "message": "Product not found"})
+

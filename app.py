50f515f18b59056a7aa4813bde7963fa08b56fef
Esteban Helguero <esteban.helguero@redk.net>
2021-11-02 01:24:34 +0100

    Get product api

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+from products import products
+
 @app.route('/')
 def home():
     return 'Flask with docker 1!'
+
+@app.route('/products')
+def getProducts():
+    return jsonify({ "products": products})
+
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [product for product in products if product["name"] == product_name]
+    if(len(productsFound) > 0):
+        return jsonify({ "products": productsFound})
+    else:
+        return jsonify({ "message": "Product not found"})
---
+++ products.py
@@ -0,0 +1,11 @@
+products = [
+    {
+        "name": "Nike Shoes XXL", "price": 35, "quantity": 4
+    },
+    {
+        "name": "Adidas Predator", "price": 54, "quantity": 4
+    },
+    {
+        "name": "Puma", "price": 32.5, "quantity": 4
+    },
+]
